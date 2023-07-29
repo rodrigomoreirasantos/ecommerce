e5047c7ad05c03310c394f0c6df74d0d2cf16289
Rodrigo Moreira Santos <rodrigomoreirasantos01@gmail.com>
2023-07-28 20:34:13 -0400

    feat: change global styles and Poppins font

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { type FunctionComponent } from 'react'
+
+interface AppProps {
+  message?: string
 }
 
-export default App;
+const App: FunctionComponent<AppProps> = ({ message }) => {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+    </div>
+  )
+}
+
+export default App
