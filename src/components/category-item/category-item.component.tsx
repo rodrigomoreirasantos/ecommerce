2ee9c790c19f322ab5d81450c2aa37752b891204
rodrigo <rodrigomoreirasantos01@gmail.com>
2023-08-01 11:31:48 -0400

    feat: add Category Item Component

---
+++ src/components/category-item/category-item.component.tsx
@@ -0,0 +1,23 @@
+import { FunctionComponent } from 'react'
+
+//Utileties
+import Category from '../../types/category.types'
+
+// Styles
+import './category-item.styles.css'
+
+interface CategoryItemProps {
+  category: Category
+}
+
+const CategoryItem: FunctionComponent<CategoryItemProps> = ({ category }) => {
+  return (
+    <div className="category-item-container" style={{backgroundImage: category.imageUrl}}>
+      <div className="category-name">
+        <p>{category.displayName}</p>
+        <p>Explore</p>
+      </div>
+    </div>
+  )
+}
+export default CategoryItem
--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -1,9 +1,11 @@
+import Categories from '../../components/categories/categories.component'
 import Header from '../../components/header/header.component'
 
 const HomePage = () => {
   return (
     <div>
       <Header />
+      <Categories />
     </div>
   )
 }
