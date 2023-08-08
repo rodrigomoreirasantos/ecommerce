e16d7d290f24540e34f74806aa45bc5263ec71f8
rodrigo <rodrigomoreirasantos01@gmail.com>
2023-08-08 10:21:25 -0400

    chore: add Firebase configuration

---
+++ src/components/config/firebase.config.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDy-73s7Vg0-X9c7vMe2eF_RKwl_6jKxjc',
+  authDomain: 'ecommerce-4800e.firebaseapp.com',
+  projectId: 'ecommerce-4800e',
+  storageBucket: 'ecommerce-4800e.appspot.com',
+  messagingSenderId: '899614804238',
+  appId: '1:899614804238:web:9dc89222f3cc259009ef5f'
+}
+
+export const app = initializeApp(firebaseConfig)
+export const db = getFirestore(app)
