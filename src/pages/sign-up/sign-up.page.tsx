9189188d3359685b9689817838e723d32fcf1699
rodrigo <rodrigomoreirasantos01@gmail.com>
2023-08-09 10:12:42 -0400

    feat: add account creation firebase validations

--- src/pages/sign-up/sign-up.page.tsx
+++ src/pages/sign-up/sign-up.page.tsx
@@ -13,7 +13,11 @@ import {
   SignUpInputContainer
 } from './sign-up.styles'
 import InputErrorMessage from '../../components/input-error-message/input-error-message.component'
-import { createUserWithEmailAndPassword } from 'firebase/auth'
+import {
+  AuthError,
+  AuthErrorCodes,
+  createUserWithEmailAndPassword
+} from 'firebase/auth'
 import { auth, db } from '../../config/firebase.config'
 import { addDoc, collection } from 'firebase/firestore'
 
@@ -30,8 +34,11 @@ const SignUpPage = () => {
     register,
     handleSubmit,
     watch,
+    setError,
     formState: { errors }
   } = useForm<SignUpForm>()
+
+  const watchPassword = watch('password')
 
   const handleSubmitPress = async (data: SignUpForm) => {
     try {
@@ -48,12 +55,13 @@ const SignUpPage = () => {
         lastName: data.lastName
       })
     } catch (error) {
-      console.log(error)
+      const _error = error as AuthError
+
+      if (_error.code === AuthErrorCodes.EMAIL_EXISTS) {
+        return setError('email', { type: 'alreadyInUse' })
+      }
     }
   }
-
-  const watchPassword = watch('password')
-
   return (
     <>
       <Header />
@@ -95,6 +103,9 @@ const SignUpPage = () => {
                 }
               })}
             />
+            {errors?.email?.type === 'alreadyInUse' && (
+              <InputErrorMessage>Email already in use</InputErrorMessage>
+            )}
             {errors?.email?.type === 'required' && (
               <InputErrorMessage>Email is required</InputErrorMessage>
             )}
@@ -108,10 +119,13 @@ const SignUpPage = () => {
               placeholder="Password"
               type="password"
               hasError={!!errors?.password}
-              {...register('password', { required: true })}
+              {...register('password', { required: true, minLength: 6 })}
             />
             {errors?.password?.type === 'required' && (
               <InputErrorMessage>Password is required</InputErrorMessage>
+            )}
+            {errors?.password?.type === 'minLength' && (
+              <InputErrorMessage>Password must be 6 characteres</InputErrorMessage>
             )}
           </SignUpInputContainer>
           <SignUpInputContainer>
@@ -122,6 +136,7 @@ const SignUpPage = () => {
               hasError={!!errors?.passwordConfirmation}
               {...register('passwordConfirmation', {
                 required: true,
+                minLength: 6,
                 validate: (value) => {
                   return value === watchPassword
                 }
@@ -131,6 +146,9 @@ const SignUpPage = () => {
               <InputErrorMessage>
                 Password Confirmation is required
               </InputErrorMessage>
+            )}
+             {errors?.passwordConfirmation?.type === 'minLength' && (
+              <InputErrorMessage>Password must be 6 characteres</InputErrorMessage>
             )}
             {errors?.passwordConfirmation?.type === 'validate' && (
               <InputErrorMessage>
