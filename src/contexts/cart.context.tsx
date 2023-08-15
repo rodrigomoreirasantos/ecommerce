4129c5c89a9efde27c1c75e2d79dbce3237ed392
rodrigo <rodrigomoreirasantos01@gmail.com>
2023-08-15 11:02:04 -0400

    feat: handle multiple products of the same id on cart

--- src/contexts/cart.context.tsx
+++ src/contexts/cart.context.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent, createContext, useState } from 'react'
+import { createContext, FunctionComponent, useState } from 'react'
 import CartProduct from '../types/cart.types'
 import Product from '../types/product.types'
 
@@ -29,11 +29,25 @@ const CartContextProvider: FunctionComponent<ChildrenProp> = ({ children }) => {
   }
 
   const addProductToCart = (product: Product) => {
+    const productIsAlreadyInCart = products.some(
+      (item) => item.id === product.id
+    )
+
+    if (productIsAlreadyInCart) {
+      return setProducts((products) =>
+        products.map((item) =>
+          item.id === product.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        )
+      )
+    }
     setProducts((prevState) => [...prevState, { ...product, quantity: 1 }])
   }
 
   return (
-    <CartContext.Provider value={{ isVisible, products, toggleCart, addProductToCart }}>
+    <CartContext.Provider
+      value={{ isVisible, products, toggleCart, addProductToCart }}>
       {children}
     </CartContext.Provider>
   )
