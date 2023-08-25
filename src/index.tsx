00081d3282ec2c8dcefa8e7dce260ad3c5b42cfe
Rodrigo Moreira Santos <rodrigomoreirasantos01@gmail.com>
2023-08-25 10:10:36 -0400

    chore: add redux setup

--- src/index.tsx
+++ src/index.tsx
@@ -6,17 +6,21 @@ import reportWebVitals from './reportWebVitals'
 import UserContextProvider from './contexts/user.context'
 import CategoryContextProvider from './contexts/category.context'
 import CartContextProvider from './contexts/cart.context'
+import { Provider } from 'react-redux'
+import store from './store/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   // <React.StrictMode>
-  <UserContextProvider>
-    <CategoryContextProvider>
-      <CartContextProvider>
-        <App />
-      </CartContextProvider>
-    </CategoryContextProvider>
-  </UserContextProvider>
+  <Provider store={store}>
+    <UserContextProvider>
+      <CategoryContextProvider>
+        <CartContextProvider>
+          <App />
+        </CartContextProvider>
+      </CategoryContextProvider>
+    </UserContextProvider>
+  </Provider>
   // </React.StrictMode>
 )
 
